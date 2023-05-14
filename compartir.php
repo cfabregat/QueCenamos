64a85645b4f95f9684e925d4c93da862acdf3582
Cristian Mauro FABREGAT <cfabregat@webrecursos.com.ar>
2023-05-13 22:54:24 -0300

    v5

--- compartir.php
+++ compartir.php
@@ -7,6 +7,27 @@
 
 <h2>Compartir plato</h2>
 
-</table>
+<h2><p align="center">Ubicación del lugar</p></h2>
+<form> 
+    <input type="text" value="Nombre">
+    <input type="text" value="Direccion">
+    <input type="text" value="Telefono">
+
+<hr>
+<h2><p align="center">Datos del Plato</p></h2>
+
+   Nombre: <input type="text">
+   Descripcion: <input type="text">
+   Precio: <input type="text">
+   Foto: <input type="file">
+
+    <br />
+
+
+   <hr>
+
+    <p align='center'><input type="submit" value="Cargar"></p>
+
+</form>
 </body>    
 </html>
