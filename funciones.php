cf210ecc9798ec0f0bb84d926e84b125ce9185b8
Cristian Mauro FABREGAT <cfabregat@webrecursos.com.ar>
2023-06-09 22:02:14 -0300

    asdf

---
+++ funciones.php
@@ -0,0 +1,31 @@
+<?php
+
+$mysql_usuario = 'root' ;
+$mysql_clave = '' ;
+$mysql_base = 'quecenamos' ;
+$con ;
+
+function mysql_conectar(){
+    global $con, $mysql_usuario, $mysql_clave, $mysql_base ;
+    $con = mysqli_connect( '127.0.0.1', $mysql_usuario, $mysql_clave, $mysql_base) ;
+}
+function mysql_desconectar(){
+    global $con ;
+    mysqli_close( $con ) ;
+}
+
+function usuario_registrar($email,$clave){
+
+    global $con ;
+    $idusuario = "NULL" ;
+    $rol = "usuario" ;
+
+    mysql_conectar() ;
+    $email = mysqli_escape_string($con, $email);
+    $clave = mysqli_escape_string($con, $clave);
+    $sql = "INSERT INTO usuarios (idusuario, email, clave, rol) VALUES ($idusuario, '$email', '$clave', '$rol');" ;
+    mysqli_query( $con, $sql );
+    mysql_desconectar() ;
+}
+
+?>
