ef001651c6e3c21d3da3258fda1570cd14ce723f
Cristian Mauro FABREGAT <cfabregat@webrecursos.com.ar>
2023-06-10 10:22:27 -0300

    asdf

--- index.php
+++ index.php
@@ -28,9 +28,44 @@
 ?>
 <html>
 <body>
-<?php
-    require( 'encabezado.php') ;
-?>
+<table width='90%'>
+    <tr>
+    <td>
+        <h1>Que Cenamos <h4>(<?php echo isset($_SESSION['email'])?$_SESSION['email']:''; ?>)</h4></h1>
+    </td>
+    <td align='right'>
+        <button onclick="window.location='/logout.php'"">Cerrar Session</button>
+        <br />
+        <button onclick="window.location='/cambiar_clave.html'"">Cambiar clave</button>
+        <br />
+        <?php
+            if( $_SESSION['rol'] == 'gestion' ){
+        ?>
+
+        <?php
+            if( $_SERVER['REQUEST_URI'] == '/usuario.php' ){
+        ?>
+        <button onclick="window.location='/gestion.php'"">Vista de Gestion</button>        
+        <?php
+            }       
+        ?>
+
+        <?php
+            if( $_SERVER['REQUEST_URI'] == '/gestion.php' ){
+        ?>
+        <button onclick="window.location='/usuario.php'"">Vista de Usuario</button>
+        <?php
+            }
+        ?>
+
+        <?php
+            }       
+        ?>
+        <button onclick="window.location='/usuario.php'"">Vista de Usuario</button>
+
+    </td>
+    <tr>
+</table>
 
 <hr /> 
 
