9572d4ec29f3ea6c5d735b129a43ad5c9b06b22f
LucasJuarez1 <lucasajuarez13@gmail.com>
2023-07-07 01:44:26 -0300

    1

--- index.php
+++ index.php
@@ -362,15 +362,15 @@
                         <input type="hidden" name="accion" value="publicar">
                         Fecha<input type="text" name="fecha" value="<?php echo date('Y-m-d H:i:s'); ?>"><br />
                         Ubicación: 
-                            <input type="text" name="ubicacion_nombre" placeholder="Nombre" required/>
-                            <input type="text" name="ubicacion_direccion" placeholder="Direccion"required/>
-                            <input type="text" name="ubicacion_telefono" placeholder="Telefono" required/>
-                            <input type="text" name="ubicacion_redsocial" placeholder="Red Social" required/><br />
+                            <input type="text" name="ubicacion_nombre" required placeholder="Nombre" >
+                            <input type="text" name="ubicacion_direccion" placeholder="Direccion">
+                            <input type="text" name="ubicacion_telefono" required placeholder="Telefono">
+                            <input type="text" name="ubicacion_redsocial" placeholder="Red Social" ><br />
                         Plato: 
-                            <input type="text" name="nombre" value="Nombre" required/>
-                            <input type="text" name="descripcion" value="Descripcion" required/>
-                            <input type="text" name="precio" value="Precio" required/><br />
-                        Foto: <input type="file" name="foto" value="foto"required/><br />
+                            <input type="text" name="nombre" value="Nombre" >
+                            <input type="text" name="descripcion" value="Descripcion" >
+                            <input type="text" name="precio" value="Precio" ><br />
+                        Foto: <input type="file" name="foto" value="foto"><br />
                         Calificaci&oacute;n:
                             <select name="calificacion">
                             <option value='1'>Mala</option>
